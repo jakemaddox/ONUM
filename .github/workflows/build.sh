045eef429004993f56cbf2b58b2886e7d6358e82
Jake Maddox <152427178+jakemaddox@users.noreply.github.com>
2026-02-17 22:00:56 -0600

    Create build.sh

---
+++ .github/workflows/build.sh
@@ -0,0 +1,20 @@
+mkdir -p _site
+echo '<!DOCTYPE html>'
+echo '<html>'
+cat head.html
+echo '<body>'
+
+while IFS=read -r line; do
+	case $line in
+		"### "*) echo -n "$line" | sed 's/## /<h3>/g' ; echo "</h3>" ;;
+		"## "*) echo -n "$line" | sed 's/## /<h2>/g' ; echo "</h2>" ;;
+		"# "*) echo -n "$line" | sed 's/## /<h1>/g' ; echo "</h1>" ;;
+		"Written by"*) echo -n '<span class="info">'; echo -n $line; echo "</span>" ;;
+		"=> Version"*) echo -n '<span class="info">'; echo -n $line | sed 's/=> /<a href="/' | sed 's/\t/">/'; echo "</a></span>" ;;
+		"=> "*) echo -n echo -n $line | sed 's/=> /<a href="/' | sed 's/\t/">/'; echo "</a></span>" ;;
+		*) [-z $line] && echo $line || echo "<p>$line</p>"
+	esac
+done
+
+echo '</body>'
+echo '</html>'
