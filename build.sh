d686780937c874d9870eeae2619dcd3df6e089da
Jake Maddox <152427178+jakemaddox@users.noreply.github.com>
2026-02-17 22:19:22 -0600

    Change `$line` to `"$line"` in build.sh to accomodate asterisks

--- build.sh
+++ build.sh
@@ -9,10 +9,10 @@ while IFS= read -r line; do
 		"### "*) echo -n "$line" | sed 's/## /<h3>/g' ; echo "</h3>" ;;
 		"## "*) echo -n "$line" | sed 's/## /<h2>/g' ; echo "</h2>" ;;
 		"# "*) echo -n "$line" | sed 's/## /<h1>/g' ; echo "</h1>" ;;
-		"Written by"*) echo -n '<span class="info">'; echo -n $line; echo "</span>" ;;
-		"=> Version"*) echo -n '<span class="info">'; echo -n $line | sed 's/=> /<a href="/' | sed 's/\t/">/'; echo "</a></span>" ;;
-		"=> "*) echo -n echo -n $line | sed 's/=> /<a href="/' | sed 's/\t/">/'; echo "</a></span>" ;;
-		*) echo $line
+		"Written by"*) echo -n '<span class="info">'; echo -n "$line"; echo "</span>" ;;
+		"=> Version"*) echo -n '<span class="info">'; echo -n "$line" | sed 's/=> /<a href="/' | sed 's/\t/">/'; echo "</a></span>" ;;
+		"=> "*) echo -n echo -n "$line" | sed 's/=> /<a href="/' | sed 's/\t/">/'; echo "</a></span>" ;;
+		*) echo "$line"
 	esac
 done < index.gmi
 
